ae9a716d0be4ec2dda10286d1a17cc689034e918
Mehedi Titu <mh.me.mehedi@gmail.com>
2023-02-07 10:19:12 +0600

    Add to cart implement

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { FaShoppingCart } from "react-icons/fa";
 
 const Modal = ({ products }) => {
     const [currentImage, setCurrentImage] = useState("");
@@ -9,6 +10,7 @@ const Modal = ({ products }) => {
         }
     }, [products]);
 
+    const [quantity, setQuantity] = useState(1);
     if (Object.keys(products).length !== 0) {
         const productImages = products?.images;
         return (
@@ -18,17 +20,21 @@ const Modal = ({ products }) => {
                     <div className="modal-box w-11/12 max-w-5xl">
                         <div className="grid grid-cols-2 gap-10">
                             <div>
-                                <img src={currentImage} alt="" />
+                                <img
+                                    src={currentImage}
+                                    alt=""
+                                    className="h-[400px]"
+                                />
                                 <div className="grid grid-cols-6">
                                     {productImages.map((image) => (
                                         <img
-                                            onClick={(e) =>
+                                            onMouseOver={(e) =>
                                                 setCurrentImage(image)
                                             }
                                             key={image.id}
                                             src={image}
                                             alt=""
-                                            className=" cursor-pointer"
+                                            className="cursor-pointer]"
                                         />
                                     ))}
                                 </div>
@@ -41,6 +47,52 @@ const Modal = ({ products }) => {
                                     ${products.price}
                                 </p>
                                 <p className="py-4">{products.description}</p>
+                                <div className="form-control w-full max-w-xs">
+                                    <label className="label">
+                                        <span className="label-text font-bold">
+                                            Set of
+                                        </span>
+                                    </label>
+                                    <select className="select select-bordered">
+                                        <option disabled selected>
+                                            Select
+                                        </option>
+                                        <option>2</option>
+                                        <option>4</option>
+                                        <option>8</option>
+                                    </select>
+                                    <div className="mt-10">
+                                        <h2 className="mb-5 font-bold text-xl">
+                                            Quantity
+                                        </h2>
+                                        <button
+                                            className="bg-gray-400 h-12 w-12 justify-center items-center"
+                                            onClick={() =>
+                                                setQuantity((prev) =>
+                                                    prev === 1 ? 1 : prev - 1
+                                                )
+                                            }
+                                        >
+                                            -
+                                        </button>
+                                        <span className="mx-4 font-bold">
+                                            {quantity}
+                                        </span>
+                                        <button
+                                            className="bg-gray-400 h-12 w-12 justify-center items-center"
+                                            onClick={() =>
+                                                setQuantity((prev) => prev + 1)
+                                            }
+                                        >
+                                            +
+                                        </button>
+                                    </div>
+                                    <div className="mt-10">
+                                        <button className="px-7 py-2 bg-blue-400 flex justify-center items-center gap-3 text-white font-semibold rounded-md">
+                                            <FaShoppingCart /> Add to Cart
+                                        </button>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <div className="modal-action">
