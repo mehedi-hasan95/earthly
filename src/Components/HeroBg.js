190c3d8f232aa48fd6142e9629f4c8cb1586907d
Mehedi Titu <mh.me.mehedi@gmail.com>
2023-02-08 11:52:17 +0600

    Impliment Typewriter

---
+++ src/Components/HeroBg.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+const HeroBg = () => {
+    return (
+        <div className="fixedBg mt-10 grid place-content-center h-screen text-center">
+            <div className="fuck">
+                <h2 className="text-9xl text-green-700 font-extrabold">
+                    WE ARE <br />
+                    <span>
+                        <Typewriter
+                            options={{
+                                strings: ["AWESOME"],
+                                autoStart: true,
+                                loop: true,
+                            }}
+                        />
+                    </span>
+                </h2>
+            </div>
+        </div>
+    );
+};
+
+export default HeroBg;
--- src/Pages/Shop/Shop.js
+++ src/Pages/Shop/Shop.js
@@ -1,14 +1,16 @@
 import React from "react";
+import HeroBg from "../../Components/HeroBg";
 import NavMenu from "../../Components/NavMenu";
 import Paralax from "../../Components/Paralax";
 import Products from "../../Components/Products";
 
 const Shop = () => {
     return (
-        <div className="">
+        <div>
             <NavMenu />
             <Products />
             <Paralax />
+            <HeroBg />
         </div>
     );
 };
